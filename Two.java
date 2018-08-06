83eabc4510a9fddbe297c143825cd8c6e71e5463
christinalljoice <42136167+christinalljoice@users.noreply.github.com>
2018-08-06 14:21:03 +0530

    Create Two.java

---
+++ Two.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+import java.util.*;
+
+class Two
+
+{
+
+public static void main(String args[])
+
+{
+
+Scanner s=new Scanner(System.in);
+
+System.out.println("Enter a number");
+
+int a=s.nextInt();
+
+if(a%2==0)
+
+{
+
+System.out.println("Even");
+
+}
+
+else 
+
+{
+
+System.out.println("Odd");
+
+}
+
+}
+}
